59fd16cd715c36042a9ca04ad677a109fbb00123
Akshay dev <akshaynrti@gmail.com>
2020-08-20 11:13:02 +0530

    first

---
+++ code1.py
@@ -0,0 +1,7 @@
+def iq_test(a):
+    a=[int(i) for i in a.split(" ")]
+    a=['e' if i%2==0 else 'o' for i in a]
+    if a.count('o')==1:
+        return a.index('o')+1
+    else:
+        return a.index('e')+1
